031284b1b123555d1b279f50791e3ed54491a3c0
Amber Jain <jainanuamber@gmail.com>
2025-11-24 12:45:36 +0530

    feat: Integrate `heroui-native` UI library by adding provider, styles, and a sample button.

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,10 @@
 import { Text, TouchableOpacity, View } from "react-native";
+import { Button } from 'heroui-native';
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-
-      <TouchableOpacity>
-        <Text
-          className="text-7xl font-bold text-gray-900 dark:text-white"
-          selectionColorClassName="accent-blue-500"
-        >
-          {`Hello World`}
-        </Text></TouchableOpacity>
+    <View className="flex-1 justify-center items-center bg-background">
+      <Button onPress={() => console.log('Pressed!')}>Get Started</Button>
     </View>
   );
 }
