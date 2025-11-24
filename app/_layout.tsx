0b7f574e07c7bb2be6f552e651cfd2200dacb1f0
Amber Jain <jainanuamber@gmail.com>
2025-11-24 13:09:31 +0530

    feat: integrate Clerk for authentication by adding dependencies and wrapping the root layout, while removing unused imports from the index screen.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,29 @@
 import "@/global.css";
 
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from "expo-router";
 import { HeroUINativeProvider } from "heroui-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+
+const RootStack = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+  console.log(isLoaded);
+  return (
+    <Stack />
+  );
+}
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <HeroUINativeProvider>
-        <Stack />
+        <ClerkProvider tokenCache={tokenCache}>
+          <RootStack />
+        </ClerkProvider>
       </HeroUINativeProvider>
     </GestureHandlerRootView>
   );
 }
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, TouchableOpacity, View } from "react-native";
 import { Button } from 'heroui-native';
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-background">
+    <View className="flex-1 justify-center items-center ">
       <Button onPress={() => console.log('Pressed!')}>Get Started</Button>
     </View>
   );
