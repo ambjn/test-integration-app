ac2de61b6ff556b7e8939d57fe881d1208c03884
Amber Jain <jainanuamber@gmail.com>
2025-11-24 12:34:10 +0530

    uniwind setup completed

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,7 @@ import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 justify-center items-center">
       <Text>Test Integration App</Text>
       <Button title="Test Integration App" onPress={() => console.log("Test Integration App")} />
     </View>
