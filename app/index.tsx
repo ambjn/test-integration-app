0420280eda247b10b87d628c6ed09bb2e6a764cc
Amber Jain <jainanuamber@gmail.com>
2025-11-24 12:38:14 +0530

    feat: replace Button with TouchableOpacity and add Uniwind type definitions.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
-import { Button, Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Test Integration App</Text>
-      <Button title="Test Integration App" onPress={() => console.log("Test Integration App")} />
+
+      <TouchableOpacity>
+        <Text
+          className="text-7xl font-bold text-gray-900 dark:text-white"
+          selectionColorClassName="accent-blue-500"
+        >
+          {`Hello World`}
+        </Text></TouchableOpacity>
     </View>
   );
 }
