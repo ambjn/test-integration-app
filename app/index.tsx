c68b8dcb145854f6f1a65e925a37829f953dccb7
Amber Jain <jainanuamber@gmail.com>
2025-11-24 12:20:25 +0530

    feat: Add a test integration button to the UI and introduce new UI-related dependencies.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Test Integration App</Text>
+      <Button title="Test Integration App" onPress={() => console.log("Test Integration App")} />
     </View>
   );
 }
